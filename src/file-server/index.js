44eb1ef98c1dbe3a10b20a7291a98dc1ee3f8e3e
kurono <kurono@riseup.net>
2024-09-25 14:55:25 -0600

    improve comments and clean the repo

--- src/file-server/index.js
+++ src/file-server/index.js
@@ -1,25 +1,14 @@
+// Import the required libraries
 const express = require('express');
-const multer  = require('multer');
-const os = require('os');
 var cors = require('cors');
 
-const upload = multer({ dest: os.tmpdir() });
-
 const app = express();
-// Enable CORS for all domains (or specify the React app's origin)
+// Enable CORS for local test
 app.use(cors({
     origin: 'http://localhost:8080'
   }));
+
+// Allow to download files from the zk directory
 app.use(express.static('./../zkproof'));
 
-app.post('/upload', upload.single('files'), function(req, res) {
-    const title = req.body.title;
-    const file = req.file;
-  
-    console.log(title);
-    console.log(file);
-  
-    res.status(200).json({message: "Successfully Uploaded", status: 200})
-  });
-
-app.listen(8000, () => console.log('Serving at http://localhost:8000!'))
+app.listen(8000, () => console.log('Serving at http://localhost:8000!'));
